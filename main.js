2a965457fccbd278d1647e410d667cb48a6156e3
cllojan <cesar.lojan8@gmail.com>
2022-09-13 22:43:06 -0500

    modal

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-
+let imgModal;
 const route  = (event) => {
     event = event || window.event;    
     event.preventDefault();
@@ -18,14 +18,33 @@ const routes = {
     
 }
 
-const  handleLocation = async () => {
+const handleLocation = async () => {
     const path = window.location.pathname;
     const route = routes[path] || routes[404];
     const html = await fetch(route)
                         .then(data => data.text());
     document.getElementById("content").innerHTML = html;
+    
 }
 
 window.onpopstate = handleLocation;
 window.route = route;
 handleLocation();
+
+
+let modal = document.getElementById("modal");
+
+function imgClick(evt){
+    console.log(evt);
+    console.log(document.querySelector("content"));
+    let img = document.getElementById("img_m");
+    console.log(img);
+    modal.style.visibility = "visible";
+}
+
+modal.addEventListener("click", (evt) => {
+    evt.stopPropagation();
+    evt.target.style.visibility = "hidden"; 
+});
+
+
