9f41845115d8efce8896840aabfe23f77a48fc60
cllojan <cesar.lojan8@gmail.com>
2022-09-20 14:29:41 -0500

    tabs

--- main.js
+++ main.js
@@ -2,41 +2,11 @@ let modal = document.getElementById("modal");
 
 let imgPro = document.getElementById("imgPro");
 let imgModal;
-const route  = (event) => {
-    event = event || window.event;    
-    event.preventDefault();
-    window.history.pushState({},"",event.target.href);
-    handleLocation();
-    var current = document.getElementsByClassName("active");
-    current[0].classList.remove("active");
-}
 
-const routes = {
-    404:"/pages/NotFound.html",
-    "/":"/pages/proyectos.html",
-    "/habilidades":"/pages/habilidades.html"
-    
-}
+
 
 const handleLocation = async () => {
 
-  
-    const path = window.location.pathname;
-    const route = routes[path] || routes[404];
-    const html = await fetch(route)
-          .then(data => data.text());
-
-  //  console.log(path);
-    if (path == "/"){
-
-	document.getElementById("tab_pro").classList.add("active");
-    }
-    if(path == "/habilidades"){
-
-	document.getElementById("tab_hab").classList.add("active");
-
-    }
-    
 
     document.getElementById("content").innerHTML = html;
     document.getElementById("ability") || document.getElementById("proyect").childNodes.forEach(elm => {
@@ -48,9 +18,6 @@ const handleLocation = async () => {
     
 }
 
-window.onpopstate = handleLocation;
-window.route = route;
-handleLocation();
 
 
 modal.addEventListener("click", (evt) => {
