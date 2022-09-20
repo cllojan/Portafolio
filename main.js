c7194c7d39f9728ee4a5a55084c206424c83fb76
cllojan <cesar.lojan8@gmail.com>
2022-09-20 16:44:51 -0500

    tabs sin router

--- main.js
+++ main.js
@@ -1,8 +1,39 @@
 let modal = document.getElementById("modal");
 
 let imgPro = document.getElementById("imgPro");
+let pro = document.getElementById("proyect");
+let hab = document.getElementById("ability");
 let imgModal;
+let prevtBtn = null
+let tabs = document.getElementById("tabs");
+tabs.addEventListener("click", (evt) => {
+    evt.preventDefault()
+    
+    if(evt.target.className != ""){
+	let current = document.getElementsByClassName("active");
+	current[0].classList.remove("active");
 
+
+	if(prevtBtn !== null){
+	    prevtBtn.classList.remove("active");
+	}
+	evt.target.classList.add("active");
+
+	
+	prevtBtn = evt.target;
+
+	if(evt.target.classList[1] == "pro"){
+	    console.log("s")
+	    pro.style.display = "grid";
+	    hab.style.display = "none";
+	}
+	if(evt.target.classList[1] == "hab"){
+	    pro.style.display = "none";
+	    hab.style.display = "flex";
+	}
+    }
+
+})
 
 
 const handleLocation = async () => {
